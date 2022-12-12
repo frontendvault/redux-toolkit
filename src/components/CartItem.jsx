2f3b1527552ae18ef737e5d4cc7032644104f145
manoj pathania <gudakesa@localhost.localdomain>
2022-12-12 15:29:32 +0530

    increase, decrease, remove  actions dispatched

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { clearCart } from "../features/cart/cartSlice";
+import { removeItem, increase, decrease } from "../features/cart/cartSlice";
 
 const CartItem = ({ id, title, imgUrl, price, amount }) => {
   const dispatch = useDispatch();
@@ -11,16 +11,33 @@ const CartItem = ({ id, title, imgUrl, price, amount }) => {
         <h3 className="my-2">{title}</h3>
         <p>{price}</p>
         <div className="d-grid">
-          <button className="btn btn-danger  text-center">
+          <button
+            className="btn btn-danger  text-center"
+            onClick={() => dispatch(removeItem(id))}
+          >
             Remove from cart
           </button>
         </div>
       </div>
       <div className="col bg-light border mt-3">
         <div className="d-flex justify-content-between">
-          <button className="col btn btn-sm btn-secondary">+</button>
+          <button
+            className="col btn btn-sm btn-secondary"
+            onClick={() => {
+              dispatch(increase({ id }));
+            }}
+          >
+            +
+          </button>
           <div className="col text-center">{amount}</div>
-          <button className="col btn btn-sm btn-secondary">-</button>
+          <button
+            className="col btn btn-sm btn-secondary"
+            onClick={() => {
+              dispatch(decrease({ id }));
+            }}
+          >
+            -
+          </button>
         </div>
       </div>
     </article>
